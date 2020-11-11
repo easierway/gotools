08649e1fad8c7b56622a78d5519a33fb45362e50
chaocai <chaocai@localhost>
2020-11-11 17:29:22 +0800

    to be threadsafe

--- mem_cache.go
+++ mem_cache.go
@@ -1,6 +1,7 @@
 package gotools
 
 import (
+	"sync"
 	"time"
 )
 
@@ -13,29 +14,53 @@ type memItem struct {
 // MemCache is the simple memory cache
 type MemCache struct {
 	cached map[string]*memItem
+	rwLock *sync.RWMutex
+}
+
+func (m *MemCache) startCleanExpiredItem() {
+	go func() {
+		for {
+			m.rwLock.Lock()
+			now := time.Now()
+			for k, v := range m.cached {
+				if now.After(v.lastUpdatedTime.Add(time.Second * time.Duration(v.expiredIntervalSecs))) {
+					m.cached[k] = nil
+					delete(m.cached, k)
+				}
+			}
+			m.rwLock.Unlock()
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
 }
 
 // NewMemCache is to create a memory cache
 func NewMemCache() *MemCache {
-	return &MemCache{
+	mc := &MemCache{
 		cached: make(map[string]*memItem),
+		rwLock: new(sync.RWMutex),
 	}
+	mc.startCleanExpiredItem()
+	return mc
 }
 
 // Put is to put the item to memory cache
 // expiredIntervalSecs is the expired duration (seconds)
 func (m *MemCache) Put(key string, item interface{}, expiredIntervalSecs int64) {
+	m.rwLock.Lock()
 	mItem := &memItem{
 		item:                item,
 		expiredIntervalSecs: expiredIntervalSecs,
 		lastUpdatedTime:     time.Now(),
 	}
 	m.cached[key] = mItem
+	m.rwLock.Unlock()
 }
 
 // Get the item from the cache
 // bool return value means: true = existing, false = not existing/expired
 func (m *MemCache) Get(key string) (interface{}, bool) {
+	m.rwLock.RLock()
 	mItem, ok := m.cached[key]
 	if !ok {
 		return nil, false
@@ -47,5 +72,13 @@ func (m *MemCache) Get(key string) (interface{}, bool) {
 		delete(m.cached, key)
 		return nil, false
 	}
+	m.rwLock.RUnlock()
 	return mItem.item, true
+}
+
+func (m *MemCache) Delete(key string) {
+	m.rwLock.Lock()
+	m.cached[key] = nil
+	delete(m.cached, key)
+	m.rwLock.Unlock()
 }
